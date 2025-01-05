707d63dc5947d17d1094220e28b95f628d972717
Ray Hernandez <107069990+hernandezraymondm@users.noreply.github.com>
2025-01-06 06:43:37 +0800

    update auth

--- auth.ts
+++ auth.ts
@@ -29,7 +29,16 @@ const createUser = async ({ id, name, username, email, image, bio }) => {
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
   callbacks: {
     async signIn({ account, user, profile }) {
       if (account.provider === "google" && !profile.email_verified) {
