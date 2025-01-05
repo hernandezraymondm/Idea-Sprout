4c2129d53f81df004943d84a3a87395319d79d31
Ray Hernandez <107069990+hernandezraymondm@users.noreply.github.com>
2025-01-06 07:06:03 +0800

    fix auth secret

--- auth.ts
+++ auth.ts
@@ -1,31 +1,62 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
-import { client } from "@/sanity/lib/client";
-import { writeClient } from "@/sanity/lib/write-client";
+import Google from "next-auth/providers/google";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
+import { client } from "./sanity/lib/client";
+import { writeClient } from "./sanity/lib/write-client";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+const getProviderId = (account, profile) => {
+  if (account.provider === "google") return profile.sub;
+  if (account.provider === "github") return profile.id;
+};
+
+const fetchUser = async (id) => {
+  return client
+    .withConfig({ useCdn: false })
+    .fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
+};
+
+const createUser = async ({ id, name, username, email, image, bio }) => {
+  await writeClient.create({
+    _type: "author",
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio: bio || "",
+  });
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({
-      user: { name, email, image },
-      profile: { id, login, bio },
-    }) {
-      const existingUser = await client
-        .withConfig({ useCdn: false })
-        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-          id,
-        });
+    async signIn({ account, user, profile }) {
+      if (account.provider === "google" && !profile.email_verified) {
+        return false;
+      }
+
+      const id = getProviderId(account, profile);
+      const existingUser = await fetchUser(id);
 
       if (!existingUser) {
-        await writeClient.create({
-          _type: "author",
+        await createUser({
           id,
-          name,
-          username: login,
-          email,
-          image,
-          bio: bio || "",
+          name: user.name,
+          username: account.provider === "github" ? profile.name : user.name,
+          email: user.email,
+          image: user.image,
+          bio: account.provider === "github" ? profile.bio : "",
         });
       }
 
@@ -33,12 +64,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     async jwt({ token, account, profile }) {
       if (account && profile) {
-        const user = await client
-          .withConfig({ useCdn: false })
-          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-            id: profile?.id,
-          });
-
+        const id = getProviderId(account, profile);
+        const user = await fetchUser(id);
         token.id = user?._id;
       }
 
