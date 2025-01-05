e5c11ea0a631a38e391f932b76cf2b271286c8ec
Ray Hernandez <107069990+hernandezraymondm@users.noreply.github.com>
2025-01-05 23:49:22 +0800

    Fix google signin

--- auth.ts
+++ auth.ts
@@ -1,7 +1,69 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
+import { client } from "./sanity/lib/client";
+import { writeClient } from "./sanity/lib/write-client";
+
+const getProviderId = (account, profile) => {
+  if (account.provider === "google") return profile.sub;
+  if (account.provider === "github") return profile.id;
+};
+
+const fetchUser = async (id) => {
+  return client
+    .withConfig({ useCdn: false })
+    .fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
+};
+
+const createUser = async ({ id, name, username, email, image, bio }) => {
+  await writeClient.create({
+    _type: "author",
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio: bio || "",
+  });
+};
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub, Google],
+  callbacks: {
+    async signIn({ account, user, profile }) {
+      if (account.provider === "google" && !profile.email_verified) {
+        return false;
+      }
+
+      const id = getProviderId(account, profile);
+      const existingUser = await fetchUser(id);
+
+      if (!existingUser) {
+        await createUser({
+          id,
+          name: user.name,
+          username: account.provider === "github" ? profile.name : user.name,
+          email: user.email,
+          image: user.image,
+          bio: account.provider === "github" ? profile.bio : "",
+        });
+      }
+
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const id = getProviderId(account, profile);
+        const user = await fetchUser(id);
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.id = token.id;
+      return session;
+    },
+  },
 });
